4b6da9bcffc608947234070a938a40bb147e52da
gajanan31 <52026504+gajanan31@users.noreply.github.com>
2021-05-07 19:29:28 +0530

    Add files via upload

---
+++ Assignment/Assignment_1/Assignment_1.java
@@ -0,0 +1,13 @@
+class Assignment_1{
+
+public static void main(String args[]){
+
+System.out.println("Hello");
+System.out.print("Gajanan Rakade_JH");
+}
+
+}
+
+
+//output
+//Hello Gajanan Rakade_JH
